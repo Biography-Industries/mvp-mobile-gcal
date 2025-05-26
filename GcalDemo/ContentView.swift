c0ac833665bedec1e07cbc26452037995f15980c
dezdoesit <dez.blair06@gmail.com>
2025-05-26 16:50:29 -0400

    Initial Commit

---
+++ GcalDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GcalDemo
+//
+//  Created by Dezmond Blair on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GcalDemo/GcalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GcalDemoApp.swift
+//  GcalDemo
+//
+//  Created by Dezmond Blair on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct GcalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
